aecaf0e284ff9027a4822d70a9846097ccf4cdfc
badillosoft <badillo.soft@hotmail.com>
2018-10-20 18:24:30 -0500

    v2.2.0

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import json
 import os
 # sudo python -m pip install flask-cors
 from flask_cors import CORS
+from os import walk
 
 app = Flask(__name__)
 
@@ -12,18 +13,22 @@ CORS(app)
 
 @app.route("/")
 def home():
-    	return render_template("index.html")
+    files = []
+    for (dirpath, dirnames, filenames) in walk("jobs"):
+        for file in filenames:
+            files.append(file.replace(".py", ""))
+    return render_template("index.html", files=files)
 
 @app.route("/<name>")
 def job(name):
-	data = ""
-	try:
-		data = request.args.to_dict()
-	except:
-		pass
-	cmd = "python jobs/{}.py '{}'".format(name, json.dumps(data))
-	stream = os.popen(cmd)
-	return stream.read()
+    data = ""
+    try:
+        data = request.args.to_dict()
+    except:
+        pass
+    cmd = "python jobs/{}.py '{}'".format(name, json.dumps(data))
+    stream = os.popen(cmd)
+    return stream.read()
 
 
 app.run(port=80)
